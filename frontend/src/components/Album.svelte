2569efea04aa06dc9ce9982e1adbab67e797759c
Shane Liesegang <shane@techie.net>
2021-02-11 18:30:46 +0200

    hide album button for non-logins

--- frontend/src/components/Album.svelte
+++ frontend/src/components/Album.svelte
@@ -368,12 +368,14 @@
     <div class="titleRow">
         <h2>{album.title}</h2>
         <div class="spacer"></div>
-        <div class="button map"
-            class:toggled={album.showMap}
-            on:click={() => album.showMap = !album.showMap}
-        >
-            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7"></path></svg>
-        </div>
+        {#if $isLoggedInStore}
+            <div class="button map"
+                class:toggled={album.showMap}
+                on:click={() => album.showMap = !album.showMap}
+            >
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7"></path></svg>
+            </div>
+        {/if}
     </div>
     {#if album.showMap}
         <div class="albumMap" bind:this={mapDiv}></div>
